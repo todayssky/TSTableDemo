dcf03ce2b2bf2919ac236a7889eabf24a5fe6187
todayssky <rudwns1113@gmail.com>
2022-09-01 16:59:06 +0900

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by todayssky on 2022/08/31.
+//
+
+import SwiftUI
+import TSTable
+
+struct ContentView: View {
+    @State var selection: TSTablePerson?
+    var body: some View {
+        VStack {
+            TSTableView(
+                elements: TSTablePerson.getMock(),
+                TSTableColumn(name: "name", value: \.givenName),
+                TSTableColumn(name: "familyName", value: \.familyName),
+                TSTableColumn(name: "emailAddress", value: \.emailAddress, widthRatio: 0.5)
+            )
+            .font(.footnote)
+            .padding()
+            
+            TSTableView(
+                elements: TSTablePerson.getMock(),
+                selection: $selection,
+                TSTableColumn(name: "name", value: \.givenName),
+                TSTableColumn(name: "familyName", value: \.familyName),
+                TSTableColumn(name: "emailAddress", value: \.emailAddress, widthRatio: 0.5)
+            )
+            .font(.callout)
+            .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/TSTableDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TSTableDemoApp.swift
+//  Shared
+//
+//  Created by todayssky on 2022/08/31.
+//
+
+import SwiftUI
+
+@main
+struct TSTableDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
